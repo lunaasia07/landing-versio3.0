7152fe139d267155e1b5b7e37c139b40da1dbf28
lunaasia07 <166106882+lunaasia07@users.noreply.github.com>
2024-07-17 02:01:14 -0500

    Add files via upload

--- julio/js/link.js
+++ julio/js/link.js
@@ -4,7 +4,7 @@
 
 
 // script.js
-const url = "NEXT MODEL";
+const url = "next";
 
 document.querySelectorAll('.enlace-reutilizable').forEach(function(enlace) {
     enlace.href = url; // Asigna la URL almacenada en la variable url al atributo href del enlace
@@ -12,7 +12,7 @@ document.querySelectorAll('.enlace-reutilizable').forEach(function(enlace) {
 
 // script.js
 document.getElementById('thumbnail').addEventListener('click', function() {
-    window.location.href = 'NEXT MODEL';
+    window.location.href = ' next';
 });
 
 
